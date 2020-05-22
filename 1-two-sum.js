a9a7d14180f5a639337bfd67e9a198547434abac
Sandy <sandy.jernigan@gmail.com>
2020-05-21 22:22:22 -0400

    Solve 1

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -19,5 +19,16 @@
  */
 var twoSum = function(nums, target) {
   // return 2 numbers from array to add up to target
-  
+  // interate over the array of numbers for the first number
+  for (i = 0; i < (nums.length - 1); i++) {
+    // interate again for the second number (cannot be the same as i)
+    for (j = i + 1; j < nums.length; j++) {
+      // get 2 numbers, check if equal to target
+      if (nums[i] + nums[j] == target) {
+        return [nums[i], nums[j]]
+      }
+    }
+  }
+  // no result will return undefined
 };
+
